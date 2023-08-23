eeda87d5c865dfb95f62bc08b52f91e9cf52e808
Otabek <130212163+ARTXxXotw@users.noreply.github.com>
2023-08-23 12:33:07 +0500

    wdawd
    addad

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import StripeContainer from './components/StripeContainer';
+import { useState } from 'react';
 
 function App() {
+
+  const [showitem,setShowItem]=useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Spatula Store</h1>
+     {showitem?<StripeContainer/>:<><h3>$10.00</h3><img src={logo} alt="" /><button onClick={()=>setShowItem(true)}>Purchase Spatula</button></>}
     </div>
   );
 }
--- web/src/components/StripeContainer.js
+++ web/src/components/StripeContainer.js
@@ -1,18 +1,16 @@
-import React from 'react'
 import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
+import React from "react"
+import PaymentForm from "./PaymentForm"
+
+const PUBLIC_KEY = "pk_test_51NhvTDERtEuSyJf8o89x0ua5W0m2G5ayuHAxCQyvOhemgG9CHs2itZqdmVYJATSLoHuN4rUxftCFNkNcw5wVelFg00BI2WQsOo"
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
 export default function StripeContainer() {
-
-
-
-    const PUBLIC_KEY=""
-
-    const stripeTestPromise=loadStripe(PUBLIC_KEY)
-
-  return (
-    <Elements stripe={stripeTestPromise}>  
-    <PaymentForm/>
-    </Elements>
-  )
+	return (
+		<Elements stripe={stripeTestPromise}>
+			<PaymentForm />
+		</Elements>
+	)
 }
